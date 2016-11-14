de41613d2234b1583245f1bdfb9ac676e30f3dfc
C. Titus Brown <titus@idyll.org>
2016-11-14 09:08:53 -0800

    fix

--- ex1/dodo.py
+++ ex1/dodo.py
@@ -1,7 +1,7 @@
 from doit.tools import run_once
 
 def task_trim_reads():
-    reads = 'simple-genome-reads.fa.gz'
+    reads = '../simple-genome-reads.fa.gz'
     memory = 1e7
     
     CMD_trim = 'trim-low-abund.py -Z 20 -C 3 -M {1} -k 31 {0}'
